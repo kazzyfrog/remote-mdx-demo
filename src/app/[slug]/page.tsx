27ddde9daed2daebcffd2a33698396e11d9b955a
kazzyfrog <70629747+kazzyfrog@users.noreply.github.com>
2024-07-11 21:34:53 +0900

    :gear: chore: read file md add mdx

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -19,16 +19,29 @@ export default async function DynamicTestPage({
 
 // rootディレクトリの_posts/test/{slug}.mdxを取得
 import fs from "fs";
+import path from "path";
 const getPostData = async (slug: string) => {
-  const filePath = `./_posts/test/${slug}.mdx`;
+  const mdxFilePath = path.join("./_posts/test", `${slug}.mdx`);
+  const mdFilePath = path.join("./_posts/test", `${slug}.md`);
+
+  let filePath;
+
+  if (fs.existsSync(mdxFilePath)) {
+    filePath = mdxFilePath;
+  } else if (fs.existsSync(mdFilePath)) {
+    filePath = mdFilePath;
+  } else {
+    throw new Error("File not found");
+  }
+
   const fileContents = fs.readFileSync(filePath, "utf8");
   return fileContents;
 };
 
-// rootディレクトリの_posts/test/全てのファイルパスを取得
+// rootディレクトリの_posts/test/全てのファイルパスを取得（mdxとmdの混合）
 export async function generateStaticParams() {
   const filePaths = await fs.promises.readdir("./_posts/test");
   return filePaths.map((filePath) => ({
-    slug: filePath.replace(/\.mdx$/, ""),
+    slug: filePath.replace(/\.(md|mdx)$/, ""),
   }));
 }
