4edee7d1ebbce6cd79c14db017a0c69e1c9a9fe5
kazzyfrog <70629747+kazzyfrog@users.noreply.github.com>
2024-07-12 02:45:25 +0900

    :gear: chore: add compileMDX options

--- src/app/test/[slug]/page.tsx
+++ src/app/test/[slug]/page.tsx
@@ -10,9 +10,26 @@ export default async function CompileMDXTestPage({
 }) {
   const postData = await getPostData(params.slug);
 
+  // オプションで、フロントマターオブジェクトの型を定義できる
   const { content, frontmatter } = await compileMDX<{ title: string }>({
     source: postData,
-    options: { parseFrontmatter: true },
+    components: { ExampleComponent },
+    options: {
+      mdxOptions: {
+        remarkPlugins: [],
+        rehypePlugins: [],
+        // remark-rehypeに渡すオプション（オプション）；
+        // allowDangerousHtmlオプションは常にtrueに設定され、MDXノード（nodeTypes参照）が渡されます；
+        // 特に、コンテンツが英語でない場合、脚注の設定を渡したいかもしれません。
+        remarkRehypeOptions: {
+          // ヘイストツリーで生のHTMLをマークダウンで保持するかどうか (デフォルト: false)。
+          allowDangerousHtml: true,
+          footnoteLabel: "脚注",
+        },
+      },
+      // MDXからfrontmatterを解析するかどうかを示す。デフォルトは false
+      parseFrontmatter: true,
+    },
   });
 
   return (
@@ -23,12 +40,12 @@ export default async function CompileMDXTestPage({
   );
 }
 
-// rootディレクトリの_posts/test/{slug}.mdxを取得
-
 export const POSTS_PATH = path.join(process.cwd(), "_posts/test");
 
 import fs from "fs";
 import path from "path";
+
+// rootディレクトリの_posts/test/{slug}.mdxの内容を取得
 const getPostData = async (slug: string) => {
   const mdxFilePath = path.join(POSTS_PATH, `${slug}.mdx`);
   const mdFilePath = path.join(POSTS_PATH, `${slug}.md`);
@@ -47,7 +64,7 @@ const getPostData = async (slug: string) => {
   return fileContents;
 };
 
-// rootディレクトリの_posts/test/全てのファイルパスを取得（mdxとmdの混合）
+// rootディレクトリの_posts/test/全てのファイルパス（slug）を取得（mdxとmdの混合）
 export async function generateStaticParams() {
   const filePaths = await fs.promises.readdir(POSTS_PATH);
   return filePaths.map((filePath) => ({
