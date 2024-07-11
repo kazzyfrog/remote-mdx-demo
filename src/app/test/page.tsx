8ed7bc593b1ba2cfc26fd1db1e10837604cc6db8
kazzyfrog <70629747+kazzyfrog@users.noreply.github.com>
2024-07-11 20:44:52 +0900

    :gear: chore: read file at root _posts dir

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,19 +1,25 @@
-import { MDXRemote, type MDXRemoteProps } from "next-mdx-remote/rsc";
+import { MDXRemote } from "next-mdx-remote/rsc";
 
 const ExampleComponent = () => <p>Example Component</p>;
-
 const components = { ExampleComponent };
-
-interface Props {
-  mdxSource: MDXRemoteProps;
-}
 
 export default async function TestPage() {
   const mdxSource = "some *mdx* content: <ExampleComponent />";
 
+  const postData = await getPostData();
+
   return (
     <div>
       <MDXRemote source={mdxSource} components={components} />
+      <MDXRemote source={postData} components={components} />
     </div>
   );
 }
+
+// rootディレクトリの_posts/test/test.mdxを取得
+import fs from "fs";
+const getPostData = async () => {
+  const filePath = "./_posts/test/test.mdx";
+  const fileContents = fs.readFileSync(filePath, "utf8");
+  return fileContents;
+};
