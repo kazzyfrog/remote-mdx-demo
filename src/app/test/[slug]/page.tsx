193ee88b4d568485100923e968defecfcfc0b305
kazzyfrog <70629747+kazzyfrog@users.noreply.github.com>
2024-07-12 01:37:49 +0900

    :gear: chore: add CompileMDXTestPage

---
+++ src/app/test/[slug]/page.tsx
@@ -0,0 +1,50 @@
+import { MDXRemote } from "next-mdx-remote/rsc";
+
+const ExampleComponent = () => <p>Example Component</p>;
+const components = { ExampleComponent };
+
+export default async function CompileMDXTestPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const postData = await getPostData(params.slug);
+
+  return (
+    <div>
+      <MDXRemote source={postData} components={components} />
+    </div>
+  );
+}
+
+// rootディレクトリの_posts/test/{slug}.mdxを取得
+
+export const POSTS_PATH = path.join(process.cwd(), "_posts/test");
+
+import fs from "fs";
+import path from "path";
+const getPostData = async (slug: string) => {
+  const mdxFilePath = path.join(POSTS_PATH, `${slug}.mdx`);
+  const mdFilePath = path.join(POSTS_PATH, `${slug}.md`);
+
+  let filePath;
+
+  if (fs.existsSync(mdxFilePath)) {
+    filePath = mdxFilePath;
+  } else if (fs.existsSync(mdFilePath)) {
+    filePath = mdFilePath;
+  } else {
+    throw new Error("File not found");
+  }
+
+  const fileContents = fs.readFileSync(filePath, "utf8");
+  return fileContents;
+};
+
+// rootディレクトリの_posts/test/全てのファイルパスを取得（mdxとmdの混合）
+export async function generateStaticParams() {
+  const filePaths = await fs.promises.readdir(POSTS_PATH);
+  return filePaths.map((filePath) => ({
+    slug: filePath.replace(/\.(md|mdx)$/, ""),
+  }));
+}
